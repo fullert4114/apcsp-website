0c4422b2547c575e4b9d27298bf9032bf4800c7f
fullert4114 <tashaafuller@yahoo.com>
2019-06-06 11:06:20 -0700

    commit

--- actionPage.c
+++ actionPage.c
@@ -1,1 +1,41 @@
-aaa
+#include <stdio.h>
+#include <string>
+
+int main (int walks, int seeds, int naps, int fish, int hights, int mailmen, int run, int scream) 
+{
+ char animal[3] = {'B', 'i', 'r', 'd','\0'};
+   printf("You are a: %s\n", greeting );
+   return 0;
+}
+
+if (seeds + hights  == 2 || hights + scream == 2)
+{    
+ printf("You are a bird!");
+    
+  }
+ 
+
+if  (naps + run  == 2 || run + fish == 2) 
+ {
+    printf("You are a Cat!");
+ 
+  } 
+
+if (walks + mailmen == 2)
+  {
+    printf("You are a dog!");
+    
+  } 
+
+if (seeds + hights + scream == 3) 
+ {
+    printf("You are a bird!");
+ 
+  } 
+
+if (naps + fish + run == 3) 
+ {
+    printf("You are a Cat!");
+   
+  } 
+}
